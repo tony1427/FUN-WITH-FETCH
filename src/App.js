018a873a7431f8e44ebec974843f47de632c4e89
Tony Giamanco <tony1427@gmail.com>
2019-03-25 07:47:06 -0500

    Added functionality to pull from random user api

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    user: {},    
+  }
+
+  componentDidMount() {
+    console.log('The component mounted');
+    fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(json => this.setState({user: json.results[0]}));
+  }
+
+
+
   render() {
+    const { name, email, picture } = this.state.user;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <img src ={ picture ? `${picture.large}` : "Loading....."}/>
+       <h1>{ name ? `${name.first}  ${name.last}` : "Loading....." }</h1>
+       <h2>{email ? `${email}` : "Loading....." }</h2>
       </div>
     );
   }
